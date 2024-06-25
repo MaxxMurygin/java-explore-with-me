4e75a85abb4f2eb26943034aa3e215fd45fd0ffe
Maxx <maxx@aolc.ru>
2024-06-25 15:03:08 +0700

    stat_svc: refaCTOR

--- stats-server/src/test/resources/data.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO public.hits(app, uri, ip, created)
-	VALUES
-('TestServiceOne','/events/1', '10.1.1.1', '2024-01-01 00:00:01'),
-('TestServiceOne','/events/1', '10.1.1.2', '2024-01-01 00:01:01'),
-('TestServiceOne','/events/1', '10.1.1.2', '2024-01-01 00:01:02'),
-('TestServiceOne','/events/2', '10.1.1.3', '2024-01-01 00:02:01'),
-('TestServiceOne','/events/2', '10.1.1.1', '2024-01-01 00:03:01'),
-('TestServiceTwo','/events/3', '10.1.1.1', '2024-01-01 00:04:01'),
-('TestServiceTwo','/events/3', '10.1.1.1', '2024-01-01 00:05:01');
--- stats-server/stats-service/src/main/java/StatsService.java
+++ stats-server/stats-service/src/main/java/StatsService.java
@@ -1,12 +1,10 @@
-package ru.practicum.ewm;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StatsServer {
+public class StatsService {
     public static void main(String[] args) {
-        SpringApplication.run(StatsServer.class, args);
+        SpringApplication.run(StatsService.class, args);
     }
 
 }
