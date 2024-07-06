a7f0a8ce2ed914e35fcfdb5c899234fae7de524a
Maxx <maxx@aolc.ru>
2024-07-07 01:24:13 +0700

    main_svc: orc work

--- ewm-service/src/main/java/ru/practicum/ewm/common/EwmDateFormatter.java
+++ ewm-service/src/main/java/ru/practicum/ewm/common/EwmDateFormatter.java
@@ -1,6 +1,5 @@
 package ru.practicum.ewm.common;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.time.format.DateTimeFormatter;
