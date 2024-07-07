630f529d26f8d7c14d7b7ed0dfc7586abc220b39
Maxx <maxx@aolc.ru>
2024-07-07 15:38:44 +0700

    main_svc: docker comlete

--- ewm-service/src/main/java/ru/practicum/ewm/controller/PublicController.java
+++ ewm-service/src/main/java/ru/practicum/ewm/controller/PublicController.java
@@ -2,7 +2,6 @@ package ru.practicum.ewm.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
--- ewm-service/src/main/java/ru/practicum/ewm/dto/event/EventMapper.java
+++ ewm-service/src/main/java/ru/practicum/ewm/dto/event/EventMapper.java
@@ -1,6 +1,5 @@
 package ru.practicum.ewm.dto.event;
 
-import org.springframework.stereotype.Component;
 import ru.practicum.ewm.common.EwmDateFormatter;
 import ru.practicum.ewm.dto.category.CategoryMapper;
 import ru.practicum.ewm.dto.user.UserMapper;
